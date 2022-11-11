5ecbdfd3b14831a4f3277ba42ab99f88caf78fc0
kimimaula <hakimi.maula@digital-omnis.com>
2022-11-11 09:37:20 +0800

    fetch data from mirage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,18 @@
+import { useState, useEffect } from 'react';
+
 function App() {
-  return <div>{/* Complete the exercise here. */}</div>;
+  const [posts, setPosts] = useState([]);
+
+  const fetchData = async () => {
+    const rawData = await fetch('/api/posts');
+    const parsedData = await rawData.json();
+    setPosts(parsedData);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+  return <div></div>;
 }
 
 export default App;
