9f979ac3af7d89b49d876a77c8124bb6dc28cb86
kimimaula <hakimi.maula@digital-omnis.com>
2022-11-11 16:04:24 +0800

    add button and select

---
+++ src/components/button/index.js
@@ -0,0 +1,7 @@
+import { CustomBtn } from './styled';
+
+const Button = ({ label }) => {
+  return <CustomBtn>{label}</CustomBtn>;
+};
+
+export default Button;
--- src/pages/lists/index.jsx
+++ src/pages/lists/index.jsx
@@ -2,13 +2,19 @@ import { useState, useEffect } from 'react';
 
 import Spinner from 'components/spinner';
 import ListCard from 'components/listCard';
+import Button from 'components/button';
 
 import Api from 'services/api';
 
 import { ListContainer } from './styled';
 
+import Select from 'react-select';
+import makeAnimated from 'react-select/animated';
+const animatedComponents = makeAnimated();
+
 function ListsPage() {
   const [posts, setPosts] = useState([]);
+  const [categories, setCategories] = useState([]);
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
@@ -17,6 +23,16 @@ function ListsPage() {
       setLoading(true);
       try {
         const { posts } = await api.getPosts();
+        var categories = posts
+          .map((post) => post.categories)
+          .flatMap((post) => post);
+        const uniq = (items) => [...new Set(items)];
+        const categoryItems = uniq(categories.map((item) => item.name)).map(
+          (c) => {
+            return { value: c.toLowerCase(), label: c };
+          }
+        );
+        setCategories(categoryItems);
         setPosts(posts);
       } catch (error) {
         console.log('----error');
@@ -31,6 +47,13 @@ function ListsPage() {
         <Spinner />
       ) : (
         <ListContainer>
+          <Select
+            closeMenuOnSelect={true}
+            components={animatedComponents}
+            isMulti
+            options={categories}
+          />
+          <Button label="Search" />
           {posts &&
             posts?.map((p) => {
               return <ListCard key={p.id} post={p} />;
