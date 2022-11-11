9b8b9bbe337de9ce1ca602cab488e77da66cdbd3
kimimaula <hakimi.maula@digital-omnis.com>
2022-11-11 09:54:41 +0800

    add display data, removed package-lock as using yarn and installed moment for datetime object

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,13 +6,33 @@ function App() {
   const fetchData = async () => {
     const rawData = await fetch('/api/posts');
     const parsedData = await rawData.json();
-    setPosts(parsedData);
+    console.log('-----parsedData', parsedData.posts);
+    setPosts(parsedData.posts);
   };
 
   useEffect(() => {
     fetchData();
   }, []);
-  return <div></div>;
+  return (
+    <div>
+      {posts?.map((p) => {
+        return (
+          <div key={p.id}>
+            <div>{p.author.name}</div>
+            <div>{p.author.avatar}</div>
+            <div>{p.title}</div>
+            <div>{p.publishDate}</div>
+            <div>{p.sumary}</div>
+            <div>
+              {p?.categories.map((c) => {
+                return <div key={c.id}>{c.name}</div>;
+              })}
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default App;
