d813e49245376b3d7f44a669abebbbfcfc86db6d
kimimaula <hakimi.maula@digital-omnis.com>
2022-11-12 08:19:44 +0800

    update to comments and installed react paginate

--- README.md
+++ README.md
@@ -40,7 +40,7 @@ These are the minimum requirements for the exercies:
 1. Output the data in a list, including properties from the data that are appropriate for a list view. - completed
 1. Implement a category filter - this can be single or multi-select. - completed
 1. Implement pagination - this can be traditional numbered pages or "load more".
-1. Use semantic markup where possible.
+1. Use semantic markup where possible. - completed
 1. Create a responsive layout with HTML and CSS.
 
 ### Additional Exercises
@@ -48,10 +48,10 @@ These are the minimum requirements for the exercies:
 If you have time then demonstrating any of the following would be considered as a bonus:
 
 1. Use client-side routing to create a "detail" page.
-1. Persist filter state in the query string.
+1. Persist filter state in the query string. - completed
 1. Include animated transitions between application state, e.g. when filtering.
 1. Convert the application to use TypeScript instead of JavaScript.
-1. Use a CSS preprocessor or CSS-in-JS rather than plain CSS.
+1. Use a CSS preprocessor or CSS-in-JS rather than plain CSS. - completed
 
 ## Submission
 
--- src/pages/lists/index.jsx
+++ src/pages/lists/index.jsx
@@ -11,9 +11,10 @@ import Api from 'services/api';
 //styles
 import { ListContainer } from './styled';
 
-//Select components
+//External components
 import Select from 'react-select';
 import makeAnimated from 'react-select/animated';
+import ReactPaginate from 'react-paginate';
 
 // routing
 import { useLocation, useNavigate } from 'react-router-dom';
@@ -25,47 +26,78 @@ const animatedComponents = makeAnimated();
 
 function ListsPage() {
   const [displayData, setDisplayData] = useState([]);
+  const [rawData, setRawData] = useState([]);
   const [selectedCat, setSelectedCat] = useState([]);
   const [categories, setCategories] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [itemOffset, setItemOffset] = useState(0);
+
+  const itemsPerPage = 5;
+  const endOffset = itemOffset + itemsPerPage;
+  const pageCount = Math.ceil(rawData.length / itemsPerPage);
+
+  // Invoke when user click to request another page.
+  const handlePageClick = (event) => {
+    const newOffset = (event.selected * itemsPerPage) % rawData.length;
+    setItemOffset(newOffset);
+  };
+
+  //checks for uniqueness in array
+  const uniq = (arr, field) => [
+    ...new Map(arr.map((item) => [item[field], item])).values(),
+  ];
 
   const location = useLocation();
   const navigate = useNavigate();
 
   //handles updating query string
-  const handleCatQuery = async (values) => {
+  const handleCatQuery = (values) => {
     setSelectedCat(values);
+    // massages object and prepare to push to query params
     const searchParams = values.map((v) => v.label);
+    // stringifies data to query params
     const stringifiedData = queryString.stringify(searchParams, {
       arrayFormat: 'comma',
     });
-    await navigate({ search: stringifiedData });
+    // push search params
+    navigate({ search: stringifiedData });
   };
 
-  // useEffect for getting inital post and populating categories
+  //listens for changes in itemoffset to update display data
+  useEffect(() => {
+    const currentItems = rawData.slice(itemOffset, endOffset);
+    setDisplayData(currentItems);
+  }, [endOffset, itemOffset, rawData]);
+
+  // useEffect for getting inital post, populating categories and setting initial pagination data
   useEffect(() => {
     const api = new Api();
     const displayData = async () => {
       setLoading(true);
       try {
+        //getting posts from api
         const { posts } = await api.getPosts();
+        // separating categories from posts
         var categories = posts
           .map((post) => post.categories)
           .flatMap((post) => post);
-        let uniq = (arr, field) => [
-          ...new Map(arr.map((item) => [item[field], item])).values(),
-        ];
+        // massaging categories data for filter
         const categoryItems = uniq(categories, 'name').map((c) => {
           return { ...c, label: c.name, value: c.name.toLowerCase() };
         });
         setCategories(categoryItems);
+        //parsing query params with values of selected filters from query string to object
         const parsed = Object.values(queryString.parse(location.search));
+        //massaging query params object to go back into filter
         const selectItem = parsed.map((v) => {
           return { value: v.toLowerCase(), label: v };
         });
+        // updating query params ui
         setSelectedCat(selectItem);
+        //checking if there are any options selected in query params
         if (parsed.length > 0) {
           let filteredData = [];
+          //looping through posts to see which object contains selected categories
           for (let i = 0; i < posts.length; i++) {
             for (let index = 0; index < posts[i].categories.length; index++) {
               if (parsed.includes(posts[i].categories[index].name)) {
@@ -73,9 +105,22 @@ function ListsPage() {
               }
             }
           }
-          setDisplayData(uniq(filteredData, 'id'));
+          // function for ssetting pagination data
+          const currentItems = uniq(filteredData, 'id').slice(
+            itemOffset,
+            endOffset
+          );
+          //setting memory for raw data
+          setRawData(uniq(filteredData, 'id'));
+          // set memory for display data
+          setDisplayData(currentItems);
         } else {
-          setDisplayData(posts);
+          // function for ssetting pagination data
+          const currentItems = posts.slice(itemOffset, endOffset);
+          //setting memory for raw data
+          setRawData(posts);
+          // set memory for display data
+          setDisplayData(currentItems);
         }
       } catch (error) {
         console.log('----error', error);
@@ -103,6 +148,15 @@ function ListsPage() {
             displayData?.map((p) => {
               return <ListCard key={p.id} post={p} />;
             })}
+          <ReactPaginate
+            breakLabel="..."
+            nextLabel="next >"
+            onPageChange={handlePageClick}
+            pageRangeDisplayed={itemsPerPage}
+            pageCount={pageCount}
+            previousLabel="< previous"
+            renderOnZeroPageCount={null}
+          />
         </ListContainer>
       )}
     </div>
