178ebbf98088523d6cb40cb06530ad8cbcea2b54
kimimaula <hakimi.maula@digital-omnis.com>
2022-11-12 09:40:11 +0800

    add ability to call single post

--- src/mock/index.js
+++ src/mock/index.js
@@ -9,5 +9,12 @@ createServer({
     this.get('/posts', () => {
       return data;
     });
+
+    // query post with id
+    this.get('/posts/:id', (schema, request) => {
+      const id = request.params.id;
+      var post = data.posts.filter((post) => post.id === id);
+      return post;
+    });
   },
 });
--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,15 @@
 import axios from 'axios';
 
 class Api {
+  //get all posts
   getPosts = async () => {
     const response = await axios.get('/api/posts');
+    const { data } = response;
+    return data;
+  };
+  //get single post
+  getPost = async (id) => {
+    const response = await axios.get(`/api/posts/${id}`);
     const { data } = response;
     return data;
   };
